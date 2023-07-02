77027f885d29b0e79dc2803794ed77fed0d273d5
Mr. John <zippybont@gmail.com>
2023-07-01 21:52:23 -0300

    Atualização da aula1 - John
    Falando sobre => variáveis e a função print()

--- Aula-/aula1.py
+++ Aula-/aula1.py
@@ -1,5 +1,27 @@
-def printar(texto='hello world'):
-    print(texto)
+# Comando básico => print() => Saída de arquivos.
+# Mostra no terminal o objeto => print(<obj>)
 
+print('Hello, World!')
 
-printar()
+# palavras chaves para string => \n Quebra de linha \t Tab. (São os mais usados, pesquisa depois mais alguns).
+
+print('Hello,\nGabriel,\n\t teste')
+
+# Variáveis;
+# Conceito => <nome da variável> = <tipo(<valor>)>
+# Mas antes saiba que nem sempre é preciso dizer o tipo.
+# Existe => str(), int(), floart() => Como tipos -primitivos-.
+# Existe também arrays, mas isso é assunto para um próximo arquivo.
+
+inteiro = int(100)
+real = float(10.0)
+string = str('Texto')
+# Entrando em mais um conceito das strings;
+print(f'Estas são as variáveis que eu declarei;\n\t{inteiro},\n\t{real},\n\t{string}.') # formatação de string, mais chamadas como -- f-strings --;
+# Também chamadas de “strings literais formatadas” (formatted string literals), f-strings são strings com a letra f no início e chaves {} para realizar a interpolação de expressões. font: "https://pythonacademy.com.br/blog/f-strings-no-python".
+# OUTPUT =>
+
+# Estas são as variáveis que eu declarei;
+# 	100,
+# 	10.0,
+# 	Texto.
