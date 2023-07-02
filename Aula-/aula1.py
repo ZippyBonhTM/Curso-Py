af1c4fc44b864ca3fcf243b3d4c2c8110732d535
Mr. John <zippybont@gmail.com>
2023-07-02 00:35:25 -0300

    M aula001 - John
    Desenvolvi mais conteúdo no arquivo ./aula-/aula1.py/

--- Aula-/aula1.py
+++ Aula-/aula1.py
@@ -1,5 +1,6 @@
 # Comando básico => print() => Saída de arquivos.
-# Mostra no terminal o objeto => print(<obj>)
+# Mostra no terminal o objeto => print(<obj>).
+# OBS: Muito utilizado para debugar o código, ou seja, encontrar falhas e ir testando as partes do código.
 
 print('Hello, World!')
 
@@ -8,7 +9,7 @@ print('Hello, World!')
 print('Hello,\nGabriel,\n\t teste')
 
 # Variáveis;
-# Conceito => <nome da variável> = <tipo(<valor>)>
+# Conceito => <nome da variável> = <tipo(<valor>)>.
 # Mas antes saiba que nem sempre é preciso dizer o tipo.
 # Existe => str(), int(), floart() => Como tipso -primitivos-.
 # Existe também arrays, mas isso é assunto para um próximo arquivo.
@@ -25,3 +26,19 @@ print(f'Estas são as variáveis que eu declarei;\n\t{inteiro},\n\t{real},\n\t{s
 # 	100,
 # 	10.0,
 # 	Texto.
+
+# OBS: Você pode colocar qualquer valor nas váriáveis que não é declarado o tipo.
+
+inteiro2 = 1
+real2 = 1.0
+string2 = 'Texto'
+
+print(
+    f"Tipos das variáveis;"
+    f"\n\t{inteiro2} <=> {type(inteiro2)};"
+    f"\n\t{real2} <=> {type(real2)};"
+    f"\n\t{string2} <=> {type(string2)};"
+    )
+
+# A função type() mostra o tipo da variável, objeto ou array.
+# A quebra de linha no print() pode ser feita a cada fechamento de "" sem problema, isso pode ser feito em outros casos que vou falar futuramente.
