acdef5b15245779971046d642d43408f125504fb
Mr. John <zippybont@gmail.com>
2023-07-02 22:59:26 -0300

    Adicionado o primeiro exercicio

---
+++ Exercicios/ex1-resolucao.py
@@ -0,0 +1,9 @@
+nome = input('Seu nome completo.\n\t ==> ')
+cpf = input('Seu CPF:\n\t ==> ')
+endereco = input('Seu endereço.\n\t ==> ')
+idade = input('Sua idade.\n\t ==> ')
+altura = input('Sua altura.\n\t ==> ')
+telefone = input('Seu teleforne.\n\t ==> ')
+
+print('-' * 10, 'Relatório de ', nome, '-' * 10)
+print(f'Nome => {nome}\nCPF => {cpf}\nEndereço => {endereco}\nIdade => {idade}\nAltura => {altura}\nTelefone => {telefone}')
